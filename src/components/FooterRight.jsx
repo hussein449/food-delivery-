8df7a490f4e75db97006926b478dccb09f5102ff
hussein449 <145710275+hussein449@users.noreply.github.com>
2025-01-29 19:52:40 +0200

    adding the info page

--- src/components/FooterRight.jsx
+++ src/components/FooterRight.jsx
@@ -1,21 +1,34 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCirclePlus, faCircleCheck, faHeart, faCommentDots, faBookmark, faShare } from '@fortawesome/free-solid-svg-icons';
+import { faCirclePlus, faCircleCheck, faHeart, faBookmark, faShare, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 import './FooterRight.css';
 
-function FooterRight({ likes, comments, saves, shares, profilePic }) {
+function FooterRight({ likes, saves, shares, profilePic }) {
   const [liked, setLiked] = useState(false);
   const [saved, setSaved] = useState(false);
   const [userAddIcon, setUserAddIcon] = useState(faCirclePlus);
+  const [showInfoPage,setShowInfoPage ] = useState(false);
 
   const handleUserAddClick = () => {
     setUserAddIcon(faCircleCheck);
     setTimeout(() => {
       setUserAddIcon(null);
-    }, 3000); // Change the delay time (in milliseconds) as needed
+    }, 3000);
   };
-
-  // Function to convert likes count to a number
+  function InfoPage() {
+    return (
+      <div  style={{ width: '100%', height: '100px', color: 'black', position:"absolute", bottom:"15%", left:"0",  backgroundColor:"white"}}  className="info-page">
+        <h2>Info Page</h2>
+        <p>This is the content of your info page.</p>
+        <button onClick={() => setShowInfoPage(false)}>Go Back</button>
+      </div>
+    );
+  }
+  
+    const handleIconClick = () => {
+      setShowInfoPage(true);
+    };
+    
   const parseLikesCount = (count) => {
     if (typeof count === 'string') {
       if (count.endsWith('K')) {
@@ -26,7 +39,6 @@ function FooterRight({ likes, comments, saves, shares, profilePic }) {
     return count;
   };
 
-  // Function to format likes count
   const formatLikesCount = (count) => {
     if (count >= 10000) {
       return (count / 1000).toFixed(1) + 'K';
@@ -42,57 +54,55 @@ function FooterRight({ likes, comments, saves, shares, profilePic }) {
     <div className="footer-right">
       <div className="sidebar-icon">
         {profilePic ? (
-          // Displaying the user profile picture
           <img src={profilePic} className='userprofile' alt='Profile' style={{ width: '45px', height: '45px', color: '#616161' }} />
         ) : null}
-        {/* The user add icon */}
         <FontAwesomeIcon icon={userAddIcon} className='useradd' style={{ width: '15px', height: '15px', color: '#FF0000' }} onClick={handleUserAddClick}/>
       </div>
       <div className="sidebar-icon">
-        {/* The heart icon for liking */}
         <FontAwesomeIcon
           icon={faHeart}
           style={{ width: '35px', height: '35px', color: liked ? '#FF0000' : 'white' }}
           onClick={handleLikeClick}
         />
-        {/* Displaying the formatted likes count */}
         <p>{formatLikesCount(parseLikesCount(likes) + (liked ? 1 : 0))}</p>
       </div>
       <div className="sidebar-icon">
-        {/* The comment icon */}
-        <FontAwesomeIcon icon={faCommentDots} style={{ width: '35px', height: '35px', color: 'white' }} />
-        {/* Displaying the number of comments */}
-        <p>{comments}</p>
+        {/* The information icon */}
+        
+        <FontAwesomeIcon
+          icon={faInfoCircle}
+          style={{ width: '35px', height: '35px', color: 'white' }}
+          onClick={handleIconClick}
+          
+        />
+       {showInfoPage&&InfoPage()}  
+
       </div>
+      
       <div className="sidebar-icon">
         {saved ? (
-          // Displaying the bookmark icon when saved
           <FontAwesomeIcon
             icon={faBookmark}
             style={{ width: '35px', height: '35px', color: '#ffc107' }}
             onClick={() => setSaved(false)}
           />
         ) : (
-          // Displaying the bookmark icon when not saved
           <FontAwesomeIcon
             icon={faBookmark}
             style={{ width: '35px', height: '35px', color: 'white' }}
             onClick={() => setSaved(true)}
           />
         )}
-        {/* Displaying the number of saves */}
         <p>{saved ? saves + 1 : saves}</p>
       </div>
       <div className="sidebar-icon">
-        {/* The share icon */}
         <FontAwesomeIcon icon={faShare} style={{ width: '35px', height: '35px', color: 'white' }} />
-        {/* Displaying the number of shares */}
         <p>{shares}</p>
       </div>
       <div className="sidebar-icon record">
-        {/* Displaying the record icon */}
         <img src="https://static.thenounproject.com/png/934821-200.png" alt='Record Icon' />
       </div>
+      
     </div>
   );
 }
