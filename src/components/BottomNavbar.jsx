e04f9dcd10dafbb3b849c125f5f5fcad6b88faa1
hussein449 <145710275+hussein449@users.noreply.github.com>
2025-01-31 11:52:15 +0200

    testing git

--- src/components/BottomNavbar.jsx
+++ src/components/BottomNavbar.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse, faUtensils, faPlus, faInbox, fa7,faUser } from '@fortawesome/free-solid-svg-icons';
 
+
+
 function BottomNavbar() {
   return (
       <div className="bottom-navbar">
